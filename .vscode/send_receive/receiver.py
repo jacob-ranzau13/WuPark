1f1fabbd201a3ae6fb94e9cb75e19d812320e7ad
Simon Korondi <sjkorondi@gmail.com>
2025-09-22 18:21:46 -0500

    receiver now extracts bits and provides info on individual spots

--- .vscode/send_receive/receiver.py
+++ .vscode/send_receive/receiver.py
@@ -1,5 +1,4 @@
 import socket as sock
-import struct # for extracting individual bytes
 
 MAX_BYTES = 1024 # maximum number of bytes the socket will accept
 
@@ -9,7 +8,23 @@ port = 12345
 s.connect(('127.0.0.1', port))
 
 data = s.recv(MAX_BYTES)
+bytenum = 0
+j = 0
+
 for byte in data:
-    print(byte)
+    if(bytenum == 0):
+        print("Expecting {} bytes".format(byte))
+        bytenum += 1
+    elif(bytenum == 1):
+        print("Lot number {}".format(byte))
+        bytenum += 1
+    else:
+        for i in range(8):
+            bit = (byte >> (7 - i)) & 1  # Get bit i (from most to least significant)
+            if(bit & 1):
+                print("Space {} is taken".format(1 + i + j * 8))
+            else:
+                print("Space {} is free".format(1 + i + j * 8))
+        j += 1
 
 s.close()
