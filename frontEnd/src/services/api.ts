286d0c89fdcc8ef914ace328ee5fcd07e9b3677e
Jacob Ranzau <jacob.ranzau@kochind.com>
2025-10-16 15:08:27 -0500

    no need to test greatness

--- frontEnd/src/services/api.ts
+++ frontEnd/src/services/api.ts
@@ -14,11 +14,6 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use(
   (config) => {
-    // Add auth headers if needed
-    // const token = localStorage.getItem('authToken');
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
     return config;
   },
   (error) => {
